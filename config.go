49e4d95e77d799e923822aef7034553f61cbb7fb
yatniel <yatniel@qq.com>
2023-03-17 16:30:31 +0800

    日常更新

--- config.go
+++ config.go
@@ -16,34 +16,75 @@ import (
 )
 
 var (
-	mConfig = &configModule{
-		config: configConfig{DEFAULT, Map{}},
-	}
+	ErrInvalidConfig = errors.New("Invalid config.")
+)
 
-	errInvalidConfig = errors.New("Invalid config.")
+var (
+	infraConfig = &configModule{
+		configurators: make(map[string]Configurator, 0),
+	}
 )
 
 type (
-	configConfig struct {
-		Driver  string
-		Setting Map
-	}
 	configModule struct {
-		mutex  sync.Mutex
-		config configConfig
+		mutex         sync.Mutex
+		configurators map[string]Configurator
+	}
+	configAction func(string) (Map, error)
+	Configurator struct {
+		// Name 名称
+		Name string
+		// Text 说明
+		Text string
+		// Alias 别名
+		Alias []string
+		// Action 获取配置
+		Action configAction
 	}
 )
 
+// Configurator 配置器
+func (this *configModule) Configurator(name string, config Configurator) {
+	this.mutex.Lock()
+	defer this.mutex.Unlock()
+
+	alias := make([]string, 0)
+	if name != "" {
+		alias = append(alias, name)
+	}
+	if config.Alias != nil {
+		alias = append(alias, config.Alias...)
+	}
+
+	for _, key := range alias {
+		if infra.override() {
+			this.configurators[key] = config
+		} else {
+			if _, ok := this.configurators[key]; ok == false {
+				this.configurators[key] = config
+			}
+		}
+	}
+}
+
+func (this *configModule) Adsasdf(name string, value Any) {
+	switch val := value.(type) {
+	case Configurator:
+		this.Configurator(name, val)
+	}
+}
+
 func (this *configModule) Register(name string, value Any) {
+	switch val := value.(type) {
+	case Configurator:
+		this.Configurator(name, val)
+	}
 }
 
 func (this *configModule) Configure(global Map) {
 }
 
 func (this *configModule) Initialize() {
-	if this.config.Driver == "" {
-		this.config.Driver = DEFAULT
-	}
 }
 
 func (this *configModule) Connect() {
