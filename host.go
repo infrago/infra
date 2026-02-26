7629b3f883799184b942e38d5196235071f6db81
cheearsan <cheearsan@gmail.com>
2026-02-26 13:19:46 -0800

    更新搜索模块和驱动

--- host.go
+++ host.go
@@ -12,9 +12,14 @@ type (
 
 	Host interface {
 		InvokeLocal(meta *Meta, name string, value Map) (Map, Res, bool)
+		RegisterLocal(name string, value Any)
 	}
 )
 
 func (h *bamgooHost) InvokeLocal(meta *Meta, name string, value Map) (Map, Res, bool) {
 	return core.invokeLocal(meta, name, value)
+}
+
+func (h *bamgooHost) RegisterLocal(name string, value Any) {
+	bamgoo.Register(name, value)
 }
